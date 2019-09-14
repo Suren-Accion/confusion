b35daef8675f1fbc13890f7391115f2a15278f4f
Suren <surendar.p@accionlabs.com>
2019-09-15 00:36:04 +0530

    Favorites

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -4,12 +4,14 @@ import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import About from "./AboutComponent";
 import Contact from "./ContactComponent";
+import Favorites from './FavoriteComponent';
+import Reservation from './ReservationComponent';
 import { View, Platform, Text, ScrollView, Image, StyleSheet } from 'react-native';
 import { createStackNavigator, createDrawerNavigator, DrawerItems, SafeAreaView } from 'react-navigation';
 import { Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { fetchDishes, fetchComments, fetchPromos, fetchLeaders } from '../redux/ActionCreators';
-import Reservation from './ReservationComponent';
+
 
 const mapStateToProps = state => {
   return {
@@ -138,6 +140,23 @@ const ReservationNavigator = createStackNavigator({
     })
   })
 
+const FavoritesNavigator = createStackNavigator({
+  Favorites: { screen: Favorites }
+}, {
+    navigationOptions: ({ navigation }) => ({
+      headerStyle: {
+        backgroundColor: "#512DA8"
+      },
+      headerTitleStyle: {
+        color: "#fff"
+      },
+      headerTintColor: "#fff",
+      headerLeft: <Icon name="menu" size={24}
+        iconStyle={{ color: 'white' }}
+        onPress={() => navigation.navigate('DrawerToggle')} />
+    })
+  })
+
 const CustomDrawerContentComponent = (props) => (
   <ScrollView>
     <SafeAreaView style={styles.container} forceInset={{ top: 'always', horizontal: 'never' }}>
@@ -207,7 +226,22 @@ const MainNavigator = createDrawerNavigator({
       )
     }
   },
-
+  Favorites:
+  {
+    screen: FavoritesNavigator,
+    navigationOptions: {
+      title: 'My Favorites',
+      drawerLabel: 'My Favorites',
+      drawerIcon: ({ tintColor, focused }) => (
+        <Icon
+          name='heart'
+          type='font-awesome'
+          size={24}
+          iconStyle={{ color: tintColor }}
+        />
+      ),
+    }
+  },
   Reservation:
   {
     screen: ReservationNavigator,
