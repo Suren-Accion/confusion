bd7296e9d39cfe63b8c6b5e39ad35ddd22b0ab17
Suren <surendar.p@accionlabs.com>
2019-09-25 11:28:03 +0530

    Local Notifications

--- shared/baseUrl.js
+++ shared/baseUrl.js
@@ -1,5 +1,8 @@
-//Bangalore House IP
-export const baseUrl = 'http://192.168.0.101:3001/';
+//Home IP
+//export const baseUrl = 'http://192.168.0.101:3001/';
 
-//Bangalore Office IP
+//Office IP
 //export const baseUrl = 'http://172.16.19.66:3001/'
+
+//Office - JP Nagar IP
+export const baseUrl = "192.168.171.27:3001"; 
